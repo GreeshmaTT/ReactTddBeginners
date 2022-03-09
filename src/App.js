e611a08960e45b2ba0a4c56d0de3aa55cef532cd
Greeshma Thankachan <greeshmathankachan@Greeshmas-MacBook-Pro.local>
2022-03-09 11:55:04 +0530

    Added test and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+//import React, { Component } from 'react';
+import React, { useState } from 'react';
+import NewMessageForm from './NewMessageForm';
+import MessageList from './MessageList';
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+  const handleSend = newMessage => {
+    setMessages([newMessage, ...messages]);
+  };
   return (
     <div>
+      <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
     </div>
   );
 };
